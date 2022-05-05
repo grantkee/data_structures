c591294a047d3595a02cbdb3e0a98d95003b2ab2
grantkee <grant@kee.ai>
2022-05-04 22:08:02 -0600

    basic algo: find largest num

---
+++ basic_algos/find_largest_num.py
@@ -0,0 +1,14 @@
+import sys
+
+def find_largest_number(args):
+    print(f"{args}")
+    largest = int(args[0])
+    for a in args:
+        num = int(a)
+        if num > largest:
+            largest = num
+    return largest
+
+if __name__ == "__main__":
+    largest = find_largest_number(sys.argv[1:])
+    print(f"{largest}")
